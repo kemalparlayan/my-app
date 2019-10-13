897b9ddeed25749d869ce4ebfc9d5d6f95bc730c
kemalparlayan <parlayankemal@gmail.com>
2019-10-13 12:42:15 +0300

    state li yapı

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,25 +1,41 @@
 import * as React from 'react';
-
+import './Hello.css';
 export interface Props {
     name: string;
     enthusiasmLevel?: number;
+  };
+
+  export interface State{
+      currentEnthusiasm:number ;
   }
 
-class  Hello extends React.Component<Props,object> {
+class  Hello extends React.Component<Props,State> {
+    constructor(props:Props){
+        super(props);
+        this.state={currentEnthusiasm:props.enthusiasmLevel||1}
+    }
+    onIncrement=()=>this.updateEnthusiasm(this.state.currentEnthusiasm+1);
+    onDecrement=()=>this.updateEnthusiasm(this.state.currentEnthusiasm-1);
     render(){
-        const {name ,enthusiasmLevel=1}=this.props;
+        const {name}=this.props;
         
-        if(enthusiasmLevel<=0)  {
+        if(this.state.currentEnthusiasm<=0)  {
             throw new Error('You could be a little more enthusiastic. :D');
         }     
 
     return(
         <div className="hello">
             <div className="greeting">
-                Hello {name+getExclamationMarks(enthusiasmLevel)}
+                Hello {name+getExclamationMarks(this.state.currentEnthusiasm)}
             </div>
+            <button onClick={this.onIncrement}>+</button>
+            <button onClick={this.onDecrement}>-</button>
         </div>
     );
+}
+ updateEnthusiasm(currentEnthusiasm:number)
+{
+    this.setState({currentEnthusiasm})
 }
 }
 
